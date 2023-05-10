5371ad183737771d893e9c3eb42c2f691aa318f1
BOURSAULT JEYSON <E20220604@stjo.edu>
2023-05-10 16:52:21 +0200

    commit

--- server.js
+++ server.js
@@ -2,24 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
+const dialogs = [
+    {
+        question : "salut",
+        answer : "coucou"
+    },
+    {
+        question : "ça va",
+        answer : "oui"
+    },
+    {
+        question : "quel age as tu",
+        answer : "20 ans"
+    }
+]
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+
+app.get('/', middleware, (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/api/v1/dialogs', (req, res) => {
-
-    const dialogs = [
-        {
-            question : "salut",
-            answer : "coucou"
-        },
-        {
-            question : "ça va",
-            answer : "oui"
+app.post('/api/v1/dialogs', (req, res) => {
+    console.log(req.body.question)
+    let matchFound = false;
+    dialogs.forEach(dialog => {
+        if(dialog.question === req.body.question) {
+            matchFound = true;
+            return res.status(200).json({response : dialog.answer})
         }
-    ]
+    })
+    if(!matchFound) {
+        res.status(200).json({ message: "pas de réponse à vous apporter" })
+    }
+})
 
+app.get('/api/v1/dialogs', (req, res) => {
     res.status(200).json({ message: dialogs })
 })
 
@@ -27,3 +46,9 @@ app.get('/api/v1/dialogs', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+function middleware(req, res, next) {
+    console.log('coucou')
+    next()
+}
