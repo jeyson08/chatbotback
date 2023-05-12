795d2696cee66f50cb043b2c69da474a365cbc44
Jeyson Boursault <jeyson.boursault@gmail.com>
2023-05-12 13:28:02 +0200

    cors modif4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 const dialogs = [
     {
         question : "salut",
